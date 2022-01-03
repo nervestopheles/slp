f1b909c741d5585ccdf4931d944f75afb24bc8aa
Soma Nervestopheles <nervestopheles@gmail.com>
2022-01-03 20:49:35 +0700

    snow: print str

--- src/games/snow.rs
+++ src/games/snow.rs
@@ -2,13 +2,12 @@ use rand::Rng;
 use std::io::Write;
 
 const WIDTH: usize = 120;
-const HEIGHT: usize = 36;
+const HEIGHT: usize = 32;
 
 fn show(screen: [[char; WIDTH]; HEIGHT]) {
     for (idx, string) in screen.iter().enumerate() {
-        for chr in string {
-            print!("{}", chr);
-        }
+        let s: String = string.iter().collect();
+        print!("{}", s);
         if idx == HEIGHT-1 {
             std::io::stdout().flush().expect("Output error.");
         } else {
@@ -26,27 +25,26 @@ fn gen_snow(screen: &mut [[char; WIDTH]; HEIGHT]) {
 }
 
 fn move_snow(screen: &mut [[char; WIDTH]; HEIGHT]) {
-    for j in (0..(HEIGHT)).rev() {
-        for i in 0..(WIDTH) {
-            if screen[j][i] == '*' {
-                screen[j][i] = ' ';
-                let mut dx = i;
+    for i in (0..(HEIGHT)).rev() {
+        for j in 0..(WIDTH) {
+            if screen[i][j] == '*' {
+                screen[i][j] = ' ';
+                let mut dx = j;
                 if rand::thread_rng().gen_range(0..100) % 2 == 0 {
                     if dx == WIDTH - 1 {
                         dx = 0;
                     } else {
                         dx += 1;
                     }
-                }
-                if rand::thread_rng().gen_range(0..100) % 2 == 0 {
+                } else if rand::thread_rng().gen_range(0..100) % 2 == 0 {
                     if dx == 0 {
                         dx = WIDTH - 1;
                     } else {
                         dx -= 1;
                     }
                 }
-                if j + 1 < HEIGHT {
-                    screen[j + 1][dx] = '*';
+                if i + 1 < HEIGHT {
+                    screen[i + 1][dx] = '*';
                 }
             }
         }
@@ -60,6 +58,6 @@ pub fn game() {
         move_snow(&mut screen);
         gen_snow(&mut screen);
         show(screen);
-        std::thread::sleep(std::time::Duration::from_millis(100));
+        std::thread::sleep(std::time::Duration::from_millis(150));
     }
 }
