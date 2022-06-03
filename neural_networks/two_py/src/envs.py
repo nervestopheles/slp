6de3e0999da91dba181a8fd9c090ebb6db2ae9ab
Soma Nervestopheles <nervestopheles@gmail.com>
2022-06-04 01:57:49 +0700

    my new favorite envs

--- neural_networks/two_py/src/envs.py
+++ neural_networks/two_py/src/envs.py
@@ -5,13 +5,13 @@ CACHEPATH = os.getenv('CACHEPATH', './cache.bin')
 MAXERAS = os.getenv('MAXERAS', 500)  # max era loops
 
 ALPHA = os.getenv('ALPHA', 0.15)  # learning rate
-EPSILON = os.getenv('EPSILON', 0.105)  # lower error bound
+EPSILON = os.getenv('EPSILON', 0.095)  # lower error bound
 DECRAISE = os.getenv('DECRAISE', 0.99)  # alpha decraise
 
-WMSIZE = os.getenv('WMSIZE', 40)
-HMSIZE = os.getenv('HMSIZE', 40)
+WMSIZE = os.getenv('WMSIZE', 60)
+HMSIZE = os.getenv('HMSIZE', 60)
 MSIZE = WMSIZE * HMSIZE  # matrix size
 
-H1 = os.getenv('H1', 18)  # count of neurons on first hidden layer
-H2 = os.getenv('H2', 12)  # count of neurons on second hidden layer
+H1 = os.getenv('H1', 16)  # count of neurons on first hidden layer
+H2 = os.getenv('H2', 8)  # count of neurons on second hidden layer
 OL = os.getenv('OL', 10)  # count of neurons on output layer
